d08ba7216ceb2f70b71514ceecc8f346fa273ac4
diniles <dzliashko@gmail.com>
2023-06-13 12:00:45 +0300

    Init: seminar 3

---
+++ seminar3/Homework.java
@@ -0,0 +1,35 @@
+/*
+Урок 3. Коллекции JAVA: Введение
+*/
+
+package seminar3;
+
+import java.util.ArrayList;
+
+public class Homework {
+    /*
+    Пусть дан произвольный список целых чисел.
+    1) Нужно удалить из него чётные числа (void removeEven(ArrayList<Integer> list))
+    2) Найти минимальное значение (Integer findMin(ArrayList<Integer> list))
+    3) Найти максимальное значение
+    4) Найти среднее значение
+    То есть, нужно реализовать 4 метода, каждый из которых принимает список целых чисел, и делает свою работу.
+    */
+    void removeEven(ArrayList<Integer> list) {
+
+    }
+
+    Integer findMin(ArrayList<Integer> list) {
+        return 0;
+    }
+
+    Integer findMax(ArrayList<Integer> list) {
+        return 0;
+    }
+
+    Integer findAverage(ArrayList<Integer> list) {
+        return 0;
+    }
+
+
+}
