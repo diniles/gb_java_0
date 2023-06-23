0c2586c463fe16f123cd0326d311796d8c162587
Dzmitry Liashko <dzliashko@gmail.com>
2023-06-23 23:55:35 +0300

    Feat: seminar 6 class Cat

---
+++ seminar6/Cat.java
@@ -0,0 +1,48 @@
+package seminar6;
+
+import java.util.Objects;
+
+public class Cat {
+
+    private String name;
+    private int appetite;
+    private boolean satiety;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        satiety = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void eat(Plate plate) {
+        if (satiety) {
+            return;
+        }
+        satiety = plate.decreaseFood(appetite);
+    }
+
+    @Override
+    public String toString() {
+        return name + " [" + appetite + "], " + "satiety?" + satiety;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        } else if (obj instanceof Cat anotherCat) {
+            return name.equals(anotherCat.name);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+}
